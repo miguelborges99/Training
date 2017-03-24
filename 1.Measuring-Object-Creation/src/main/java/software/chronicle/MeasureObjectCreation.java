b2bd83d74df2b07ae24ec9d1df70030f4699056b
Rob Austin <rob.austin@boundedbuffer.co.uk>
2017-03-24 12:52:43 +0000

    Update MeasureObjectCreation.java

--- 1.Measuring-Object-Creation/src/main/java/software/chronicle/MeasureObjectCreation.java
+++ 1.Measuring-Object-Creation/src/main/java/software/chronicle/MeasureObjectCreation.java
@@ -8,7 +8,7 @@ public class MeasureObjectCreation {
     public static void main(String[] args) {
         Runtime.getRuntime().gc();
         long amountOfFreeMemoryInBytes = Runtime.getRuntime().freeMemory();
-        new Object();
+        // TODO create the object
         System.out.println("objectSize=" + (amountOfFreeMemoryInBytes - Runtime.getRuntime().freeMemory()));
     }
 }
