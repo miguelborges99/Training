1a3a281ed434fe40ca7ea9c0002575abb8b36121
Rob Austin <rob.austin@boundedbuffer.co.uk>
2017-03-27 15:28:08 +0100

    queue off heap

---
+++ 9.OffHeapMap/src/main/java/software/chronicle/OffHeapQueue.java
@@ -0,0 +1,42 @@
+package software.chronicle;
+
+import net.openhft.chronicle.queue.ExcerptAppender;
+import net.openhft.chronicle.queue.ExcerptTailer;
+import net.openhft.chronicle.queue.TailerDirection;
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueue;
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
+import net.openhft.chronicle.wire.DocumentContext;
+
+import java.io.File;
+
+/**
+ * @author Rob Austin.
+ */
+public class OffHeapQueue {
+
+    public static void main(String[] args) {
+        final SingleChronicleQueue q = SingleChronicleQueueBuilder.binary(new File("file.q4")).build();
+        long end = 0;
+        final ExcerptAppender appender = q.acquireAppender();
+        final ExcerptTailer tailer = q.createTailer().direction(TailerDirection.BACKWARD).toEnd();
+        try (final DocumentContext dc = tailer.readingDocument()) {
+            if (dc.isPresent())
+                end = dc.wire().getValueIn().int64();
+        }
+
+        try (final DocumentContext documentContext = appender.writingDocument()) {
+            documentContext.wire().getValueOut().int64(end + 1);
+        }
+
+
+        final ExcerptTailer direction = tailer.toStart().direction(TailerDirection.FORWARD);
+        for (; ; ) {
+            try (final DocumentContext dc = direction.readingDocument()) {
+                if (!dc.isPresent())
+                    return;
+                System.out.println(dc.wire().getValueIn().int64());
+            }
+        }
+
+    }
+}
