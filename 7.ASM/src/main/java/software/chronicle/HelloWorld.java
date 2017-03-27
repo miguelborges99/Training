55a6eed0cc77c4432f00af5d93a4d4b96134d285
Rob Austin <rob.austin@boundedbuffer.co.uk>
2017-03-27 13:18:37 +0100

    added ASM examples

---
+++ 7.ASM/src/main/java/software/chronicle/HelloWorld.java
@@ -0,0 +1,14 @@
+package software.chronicle;
+
+/**
+ * @author Rob Austin.
+ */
+public class HelloWorld {
+    public HelloWorld() {
+        System.out.println("hello world");
+    }
+
+    public static void main(String[] args) {
+        new HelloWorld();
+    }
+}
