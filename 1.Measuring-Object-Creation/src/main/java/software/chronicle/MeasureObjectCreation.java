f452b8f5448c34591a623b69f7cbb4fb08b79dad
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2017-03-28 09:54:03 +0100

    review

--- 1.Measuring-Object-Creation/src/main/java/software/chronicle/MeasureObjectCreation.java
+++ 1.Measuring-Object-Creation/src/main/java/software/chronicle/MeasureObjectCreation.java
@@ -5,10 +5,28 @@ package software.chronicle;
  */
 public class MeasureObjectCreation {
 
-    public static void main(String[] args) {
-        Runtime.getRuntime().gc();
-        long amountOfFreeMemoryInBytes = Runtime.getRuntime().freeMemory();
+    private static void createObject() {
         // TODO create the object
-        System.out.println("objectSize=" + (amountOfFreeMemoryInBytes - Runtime.getRuntime().freeMemory()));
+    }
+
+    public static void main(String[] args) {
+        System.gc();
+
+        createObject(); // warm up.
+
+        long used1 = memoryUsed();
+
+        createObject();
+
+        long used2 = memoryUsed();
+        if (used1 == used2)
+            System.err.println("You need to turn off the TLAB with -XX:-UseTLAB");
+        else
+            System.out.printf("Space used by one object is " + (used2 - used1) + " bytes%n");
+    }
+
+    public static long memoryUsed() {
+        Runtime rt = Runtime.getRuntime();
+        return rt.totalMemory() - rt.freeMemory();
     }
 }
